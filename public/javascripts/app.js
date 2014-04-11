bd4aee557f7a60c57486008da5421cb2bf7546f2
Rudolf Olah <omouse@gmail.com>
2014-04-11 00:33:36 -0400

    fixed path of main partial template

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,7 @@ angular.module('gcalInvoice', ['ngRoute', 'googleApi'])
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'public/partials/main.html',
+                templateUrl: '/partials/main.html',
                 controller: 'InvoiceController'
             })
             .otherwise({ redirectTo: '/main' });

cafa21566a9303920cc71a06374fe5da5d83324c
Rudolf Olah <omouse@gmail.com>
2014-04-11 00:47:11 -0400

    big commit, moving from the laptop to the desktop, too lazy to detail every change that happened :S :S

--- public/javascripts/InvoiceController.js
+++ public/javascripts/InvoiceController.js
@@ -2,11 +2,26 @@ angular.module('gcalInvoice').controller(
     'InvoiceController',
     ['$scope', 'googleLogin', 'googleCalendar',
      function($scope, googleLogin, googleCalendar) {
+         var selectCalendarAndFilterEvents = function() {
+             console.log('updating listed events');
+         };
+
          $scope.loggedIn = false;
+         $scope.calendars = [];
+         $scope.selectedCalendar = null;
          $scope.events = [];
+         $scope.selectedEvents = [];
+         $scope.months = ['January', 'February', 'March', 'April', 'May',
+                          'June', 'July', 'August', 'September', 'October',
+                          'November', 'December'];
+         $scope.years = [2013, 2014, 2015];
+
          $scope.login = function() {
-             googleLogin.login();
-             $scope.loggedIn = true;
+             googleLogin.login().then(function() {
+                 $scope.loggedIn = true;
+             });
+         };
+
          $scope.selectCalendar = function(calendarId) {
              googleCalendar.listEvents({
                  calendarId: calendarId
@@ -14,6 +29,12 @@ angular.module('gcalInvoice').controller(
                  $scope.events = events;
              });
          };
+
+         $scope.$on('googleCalendar:loaded', function() {
+             googleCalendar.listCalendars().then(function(calendars) {
+                 $scope.calendars = calendars;
+             });
+         });
      }
     ]
 );

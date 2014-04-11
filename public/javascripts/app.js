e30c059f3e7309e5b6b1766ac61f8f0ecf46047e
Rudolf Olah <omouse@gmail.com>
2014-04-11 00:46:43 -0400

    added google calendar API key (it's a throwaway and must be replaced in your own apps)

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,13 @@
 angular.module('gcalInvoice', ['ngRoute', 'googleApi'])
+    .config(function(googleLoginProvider) {
+        googleLoginProvider.configure({
+            clientId: '141159713889-6t2m78ce3fl53duingebjplpm26e861f.apps.googleusercontent.com',
+            scopes: [
+                'https://www.googleapis.com/auth/userinfo.email',
+                'https://www.googleapis.com/auth/calendar'
+            ]
+        });
+    })
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {

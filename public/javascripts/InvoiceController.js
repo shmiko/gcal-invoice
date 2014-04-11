1f5161f69f48e674cba8372f15ee63d0930b2c83
Rudolf Olah <omouse@gmail.com>
2014-04-11 00:34:28 -0400

    added events list and selectCalendar function to $scope of InvoiceController

--- public/javascripts/InvoiceController.js
+++ public/javascripts/InvoiceController.js
@@ -3,9 +3,16 @@ angular.module('gcalInvoice').controller(
     ['$scope', 'googleLogin', 'googleCalendar',
      function($scope, googleLogin, googleCalendar) {
          $scope.loggedIn = false;
+         $scope.events = [];
          $scope.login = function() {
              googleLogin.login();
              $scope.loggedIn = true;
+         $scope.selectCalendar = function(calendarId) {
+             googleCalendar.listEvents({
+                 calendarId: calendarId
+             }).then(function(events) {
+                 $scope.events = events;
+             });
          };
      }
     ]

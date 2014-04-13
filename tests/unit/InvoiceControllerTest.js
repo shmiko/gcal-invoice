9a99c0384a5d4e4b9acbbdc318ea8e1535770829
Rudolf Olah <omouse@gmail.com>
2014-04-13 13:06:13 -0400

    updated unit tests, how they're labeled and added beginnings of the next tests

--- tests/unit/InvoiceControllerTest.js
+++ tests/unit/InvoiceControllerTest.js
@@ -8,21 +8,42 @@ describe('Invoice Controller', function() {
         controller = $controller('InvoiceController', { $scope: scope });
     }));
 
-    it('the invoice and its update method should be defined', function() {
+    it('should define the invoice object, its update method and selected events', function() {
         assert.isObject(scope.invoice);
         assert.isFunction(scope.updateInvoice);
+        assert.isArray(scope.selectedEvents);
     });
 
-    it("the invoice hourly rate should be between 0.00 and 1000.00", function() {
+    it("should set the default invoice hourly rate between 0.00 and 1000.00", function() {
         assert.isNotNull(scope.invoice.hourlyRate);
         assert(0.00 <= scope.invoice.hourlyRate && scope.invoice.hourlyRate <= 1000.00);
     });
 
-    it('the invoice has no line items when there are no selected events', function() {
+    it('has no invoice line items when there are no selected events', function() {
         scope.selectedEvents = [];
         scope.updateInvoice();
         assert.lengthOf(scope.selectedEvents, 0);
         assert.isArray(scope.invoice.lineItems);
         assert.lengthOf(scope.invoice.lineItems, 0);
+    });
+
+    it('has 1 invoice line item when there is 1 selected event', function() {
+        assert.lengthOf(scope.selectedEvents, 1);
+        assert.lengthOf(scope.invoice.lineItems, 1);
+    });
+
+    it('has 1 invoice line item when there are 2 selected events that happen on the same day', function() {
+        assert.lengthOf(scope.selectedEvents, 2);
+        assert.lengthOf(scope.invoice.lineItems, 1);
+    });
+
+    it('has 2 invoice line items when there are 3 selected events and 2 of them occur on the same day', function() {
+        assert.lengthOf(scope.selectedEvents, 3);
+        assert.lengthOf(scope.invoice.lineItems, 2);
+    });
+
+    it('splits a selected event that takes place over 3 days into 3 invoice line items', function() {
+        assert.lengthOf(scope.selectedEvents, 3);
+        assert.lengthOf(scope.invoice.lineItems, 3);
     });
 });

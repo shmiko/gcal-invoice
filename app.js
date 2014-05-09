421823491f5857bc32830a5321e666c067789067
Rudolf Olah <omouse@gmail.com>
2014-05-09 18:27:12 -0400

    changed default port from 3000 to 3003

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ var app = express();
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/', express.static(__dirname + '/public'));
-app.listen(3000);
+app.listen(3003);
